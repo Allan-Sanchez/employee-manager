aa0831aa859f407a13957010ea09b421e7b7518a
Allan-Sanchez <asanchezrixtun@gmail.com>
2019-10-09 20:37:25 -0600

    show data firebase

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,9 +1,9 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import firebaseConfig from './firebaseConfig';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-//  firebase.analytics();
+//  firebase.analytics();   
 
 
 export default firebaseApp.firestore();
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,66 @@
 <template>
     <div id="dashboard">
        
-        <h1>Dashboard</h1>
+        <h1 class="is-size-2 has-text-centered" style="margin-top:60px">Dashboard</h1>
 
+        <!-- <ul>
+            <li v-for="employee in employees" :key="employee.id">
+                {{employee.name}}
+            </li>
+        </ul> -->
+        <!-- <b-table style="margin-top:40px" :data="employees" :columns="columns"></b-table> -->
+            <b-table :data="employees">
+                <template slot-scope="props">
+                    <b-table-column field="employee_id" label="ID" width="40" numeric>
+                        <template slot="header" slot-scope="{ column }">
+                            <b-tooltip :label="column.label" dashed>
+                                {{ column.label }}
+                            </b-tooltip>
+                        </template> 
+                        {{ props.row.employee_id }}
+                    </b-table-column>
+
+                    <b-table-column field="name" label="Name">
+                        <template slot="header" slot-scope="{ column }">
+                            <b-tooltip :label="column.label" dashed>
+                                {{ column.label }}
+                            </b-tooltip>
+                        </template>
+                        {{ props.row.name }}
+                    </b-table-column>
+
+                    <b-table-column field="dept" label="Departament">
+                        <template slot="header" slot-scope="{ column }">
+                            <b-tooltip :label="column.label" dashed>
+                                {{ column.label }}
+                            </b-tooltip>
+                        </template>
+                        {{ props.row.dept }}
+                    </b-table-column>
+                
+                    <b-table-column field="position" label="Position">
+                        <template slot="header" slot-scope="{ column }">
+                            <b-tooltip :label="column.label" dashed>
+                                {{ column.label }}
+                            </b-tooltip>
+                        </template>
+                        {{ props.row.position }}
+                    </b-table-column>
+
+                    <b-table-column label="Actions">
+                        <router-link :to="{ name:'view-employee',params:{employee_id :props.row.employee_id }}">
+                            <b-icon
+                                pack="fas"
+                                icon="eye"
+                                type="is-primary"
+                                size="is-small">
+                            </b-icon>
+                        </router-link>
+                    </b-table-column>
+
+                </template>
+            </b-table>
+        <!-- flating button -->
         <div class="btn-float">
             <a class="float" >
                 <b-icon class="my-float"
@@ -20,13 +78,31 @@
 </template>
 
 <script>
-   
+  import db from '../components/firebaseInit'; 
 export default {
     nama:'dashboard',
     data() {
         return {
+            employees:[],
             
         }
+    },
+    created() {
+        db.collection("employees").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const  data = {
+                    'id' : doc.id,
+                    'employee_id': doc.data().employee_id,
+                    'name': doc.data().name,
+                    'dept':doc.data().dept,
+                    'position':doc.data().position
+                }
+
+                this.employees.push(data);
+            });
+        });
+
+
     },
 }
 </script>
